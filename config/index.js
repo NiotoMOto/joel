71b93c11e391ab023a3b8d5785794c212400a3c6
Antoine Guillemoto <aguillemoto.trepia@tf1.fr>
2016-10-21 17:50:05 +0200

    change config mongo

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   expiresIn: 36000,
   secret:  'mysecretjwt',
   apiUrl : 'http://localhost:4000/api/v1',
-  mongoUrl : 'localhost:27017',
+  mongoUrl : 'srv.guillemoto.com:27017',
   log: {
     output: 'stdout',
     level: 'info'
