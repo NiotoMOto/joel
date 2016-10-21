0f7e42397749eafe6f2cf69f91aef97d822967bd
Antoine Guillemoto <aguillemoto.trepia@tf1.fr>
2016-10-21 11:14:59 +0200

    delete adminLTE

--- server/constants/scripts.js
+++ server/constants/scripts.js
@@ -2,7 +2,7 @@
 
 const { pick } = require('../common/services/util/object');
 
-const defaultScripts = ['jquery', 'bootstrap', 'adminLTE'];
+const defaultScripts = ['jquery', 'bootstrap'];
 
 exports.jquery = {
   src: '/vendors/jquery/dist/jquery.min.js',
@@ -10,10 +10,6 @@ exports.jquery = {
 
 exports.bootstrap = {
   src: '/vendors/bootstrap/dist/js/bootstrap.min.js',
-};
-
-exports.adminLTE = {
-  src: '/vendors/admin-lte/dist/js/app.min.js',
 };
 
 exports.defaults = pick.apply(null, [exports].concat(defaultScripts));
